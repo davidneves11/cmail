5be6f65897c9e504856a200d4cd8a1836b16c965
davidneves11 <50880764+davidneves11@users.noreply.github.com>
2020-06-16 21:46:53 -0300

    Exibição do email enviado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,6 +8,23 @@ import { Component } from '@angular/core';
 export class AppComponent {
     private	_isNewEmailFormOpen	=	false;
 
+    emailList	=	[];
+    email	=	{
+        destinatario:	'',
+        assunto:	'',
+        conteudo:	''
+    }
+  
+    handleNewEmail(event:	Event)	{
+      event.preventDefault();
+      this.emailList.push(this.email)
+      this.email	=	{
+          destinatario:	'',
+          assunto:	'',
+          conteudo:	''
+      }	
+    }
+       
     get	isNewEmailFormOpen(){
             return this._isNewEmailFormOpen;
     }
