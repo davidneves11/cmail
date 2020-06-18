38e3eb0855915eee74500c215a64779bd673878d
davidneves11 <50880764+davidneves11@users.noreply.github.com>
2020-06-17 22:51:32 -0300

    Rotas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,37 +3,7 @@ import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template:	'<router-outlet></router-outlet>',
+	styles:	[]
 })
-export class AppComponent {
-    private	_isNewEmailFormOpen	=	false;
-
-    emailList	=	[];
-    email	=	{
-        destinatario:	'',
-        assunto:	'',
-        conteudo:	''
-    }
-  
-    handleNewEmail(formEmail:	NgForm)	{
-      if(formEmail.invalid)	return;
-      
-      this.emailList.push(this.email)
-      this.email	=	{
-          destinatario:	'',
-          assunto:	'',
-          conteudo:	''
-      }	
-      formEmail.reset();
-
-    }
-       
-    get	isNewEmailFormOpen(){
-            return this._isNewEmailFormOpen;
-    }
-    
-    toggleNewEmailForm()	{
-            this._isNewEmailFormOpen	=	!this.isNewEmailFormOpen
-    }
-}
+export class AppComponent {}
