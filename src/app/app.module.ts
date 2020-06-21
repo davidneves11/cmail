f4fd435945cec09c47ddbacc68af30626e5fa6e8
davidneves11 <50880764+davidneves11@users.noreply.github.com>
2020-06-21 00:17:21 -0300

    Cadastro de contas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 
-import	{	FormsModule	}	from	"@angular/forms";
+import { FormsModule	}	from	"@angular/forms";
 import { CaixaDeEntradaComponent } from './modules/caixa-de-entrada/caixa-de-entrada.component';
 import { LoginComponent } from './modules/login/login.component';
 import { CadastroComponent } from './modules/cadastro/cadastro.component';
-import	{	ModuloRoteamento	}	from	'./app.routes';
+import { ModuloRoteamento	}	from	'./app.routes';
+import { CmailFormGroupComponent } from './components/cmail-form-group/cmail-form-group.component';
+import { CmailFormFieldDirective } from './components/cmail-form-group/cmail-form-field.directive';
 
 @NgModule({
   declarations: [
@@ -16,7 +18,9 @@ import	{	ModuloRoteamento	}	from	'./app.routes';
     HeaderComponent,
     CaixaDeEntradaComponent,
     LoginComponent,
-    CadastroComponent
+    CadastroComponent,
+    CmailFormGroupComponent,
+    CmailFormFieldDirective
   ],
   imports: [
     BrowserModule,
